03687838bd91120c82c6184e956ae338470c4b88
Allison Whitney <corvalys@gmail.com>
2018-11-29 00:31:35 -0500

    dialogue scripts

---
+++ SpookyTownHorror/Assets/Scripts/Dialogue-Cutscenes/DialogueManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public string[] dialogueOptions;
+    public Text dialogueBox;
+    public int dialogueNum;
+    public float[] speechLength;
+
+    public GameObject[] triggerOrder;
+
+    public IEnumerator showDialogue()
+    {
+        dialogueBox.text = dialogueOptions[dialogueNum];
+        yield return new WaitForSeconds(speechLength[dialogueNum]);
+        dialogueBox.text = "";
+        triggerOrder[dialogueNum].SetActive(false);
+        dialogueNum++;
+        triggerOrder[dialogueNum].SetActive(true);
+    }
+}
---
+++ SpookyTownHorror/Assets/Scripts/Dialogue-Cutscenes/DialogueTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public DialogueManager dm;
+
+	// Use this for initialization
+	void Start () {
+        dm = GameObject.Find("DialogueManager").GetComponent<DialogueManager>();
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "dialogue")
+        {
+            StartCoroutine(dm.showDialogue());
+        }
+    }
+}
