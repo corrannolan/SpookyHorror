24d9b56aecdedccfbd6d0be84d1556c422248a23
corrannolan <corrannolan3097@gmail.com>
2018-11-28 22:53:46 -0500

    hopefully a thing

---
+++ SpookyTownHorror/Assets/Scripts/Dialogue-Cutscenes/Scene1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Scene1 : MonoBehaviour {
+    public GameObject[] points;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
