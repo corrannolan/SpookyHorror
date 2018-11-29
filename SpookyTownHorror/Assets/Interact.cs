1899996aae7e713eab4ec65011d004aede39f2fb
Allison Whitney <corvalys@gmail.com>
2018-11-29 03:03:06 -0500

    interact

--- SpookyTownHorror/Assets/Interact.cs
+++ SpookyTownHorror/Assets/Interact.cs
@@ -1,16 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
-public class Interact : MonoBehaviour {
+public class Interact : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    Player player;
+    public GameObject item, playerchar, ui;
+    public bool door, key, distraction, other, interactable;
+    // Use this for initialization
+    void Start()
+    {
+
+        player = ReInput.players.GetPlayer(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interactable)
+        {
+            if (player.GetButtonDown("Hide"))
+            {
+                if (door)
+                {
+
+                }
+                else if (key)
+                {
+                    print("keys");
+                    //PlayerItems.keys++;
+                }
+                else if (distraction)
+                {
+                    //PlayerItems.distract = true;
+                }
+                else if (other)
+                {
+
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ui.SetActive(true);
+        interactable = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        ui.SetActive(false);
+        interactable = false;
+    }
 }
