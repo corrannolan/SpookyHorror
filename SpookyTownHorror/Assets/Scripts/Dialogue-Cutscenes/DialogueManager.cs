83ea326177f7cee4fe54b899f53e7f0984cee0e2
Allison Whitney <corvalys@gmail.com>
2018-11-29 04:29:36 -0500

    trying to put in tutorial...

--- SpookyTownHorror/Assets/Scripts/Dialogue-Cutscenes/DialogueManager.cs
+++ SpookyTownHorror/Assets/Scripts/Dialogue-Cutscenes/DialogueManager.cs
@@ -11,12 +11,15 @@ public class DialogueManager : MonoBehaviour {
     public float[] speechLength;
 
     public GameObject[] triggerOrder;
+    public GameObject panel;
 
     public IEnumerator showDialogue()
     {
+        panel.SetActive(true);
         dialogueBox.text = dialogueOptions[dialogueNum];
         yield return new WaitForSeconds(speechLength[dialogueNum]);
         dialogueBox.text = "";
+        panel.SetActive(false);
         triggerOrder[dialogueNum].SetActive(false);
         dialogueNum++;
         triggerOrder[dialogueNum].SetActive(true);
---
+++ SpookyTownHorror/Assets/Tutorial.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class Tutorial : MonoBehaviour {
+
+    public int playerID;
+    private Player player;
+
+    public string tutorialTopic;
+
+    public GameObject crouchUI, pickUpUI, hideUI, distractUI;
+    private PlayerMovement pm;
+
+    private CharacterMovement cm;
+
+	// Use this for initialization
+	void Start () {
+        pm = GameObject.Find("PlayerMove").GetComponent<PlayerMovement>();
+        cm = GameObject.Find("Theo").GetComponent<CharacterMovement>();
+
+        player = ReInput.players.GetPlayer(playerID);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.GetButtonDown("Crouch") && pm.canMove == false)
+        {
+            cm.MoveNext();
+            pm.canMove = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            if (tutorialTopic == "crouch")
+            {
+                pm.canMove = false;
+                crouchUI.SetActive(true);
+                
+            }
+            else if (tutorialTopic == "pickUp")
+            {
+
+            }
+            else if (tutorialTopic == "hide")
+            {
+
+            }
+            else if (tutorialTopic == "distract")
+            {
+
+            }
+        }
+        
+    }
+}
