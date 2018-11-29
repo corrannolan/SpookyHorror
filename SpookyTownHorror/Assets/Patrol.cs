0724416be04d2488fd8e1c993f4a7ae3893405eb
Damon Stallman <dstallman@mica.edu>
2018-11-28 23:42:09 -0500

    enemy script in scene

---
+++ SpookyTownHorror/Assets/Patrol.cs
@@ -0,0 +1,39 @@
+﻿// Patrol.cs
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class Patrol : MonoBehaviour
+{
+
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GotoNextPoint();
+    }
+
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+        {
+            return;
+        }
+
+        agent.destination = points[destPoint].position;
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        if (agent.remainingDistance < 0.1f)
+        {
+            GotoNextPoint();
+        }
+    }
+}
