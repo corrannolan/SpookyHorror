5f27573889621e3966de4e2d45e91f8beea2c01e
corrannolan <corrannolan3097@gmail.com>
2018-11-28 23:59:25 -0500

    character movement script

---
+++ SpookyTownHorror/Assets/Scripts/Dialogue-Cutscenes/CharacterMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterMovement : MonoBehaviour {
+    public NavMeshAgent nM;
+
+    public GameObject[] points;
+
+    public int currentPoint = 0;
+    Transform desPoint;
+
+    bool stopped = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        desPoint = points[0].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (stopped != true)
+        {
+            nM.isStopped = false;
+        }
+
+        if (nM.remainingDistance < 0.1f && stopped == false)
+        {
+            stopped = true;
+            nM.isStopped = true;
+
+            NextPoint();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        nM.destination = desPoint.position;
+    }
+
+    void NextPoint()
+    {
+        currentPoint++;
+        desPoint = points[currentPoint].transform;
+    }
+
+    public void MoveNext()
+    {
+        stopped = false;
+    }
+}
