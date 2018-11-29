197f05d250d05e8f4b20cc165bffbbb6d440228f
corrannolan <corrannolan3097@gmail.com>
2018-11-29 14:34:50 -0500

    pls wqork fremb

---
+++ SpookyTownHorror/Assets/Scripts/Dialogue-Cutscenes/PlayerItems.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerItems : MonoBehaviour {
+
+    public static int keys;
+    public static bool distract;
+	// Use this for initialization
+	void Start () {
+        distract = false;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(keys);
+        if (distract)
+        {
+            print("distracts");
+        }
+	}
+}
