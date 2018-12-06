4c040c71dd2207e62600d556f95373a6be188408
Allison Whitney <corvalys@gmail.com>
2018-12-06 13:52:30 -0500

    i'm a fence

--- SpookyTownHorror/Assets/PlayAnimations.cs
+++ SpookyTownHorror/Assets/PlayAnimations.cs
@@ -18,15 +18,12 @@ public class PlayAnimations : MonoBehaviour {
     // Update is called once per frame
     void Update() {
 
-<<<<<<< HEAD
         if (player.GetAxis("MoveV") != 0 || player.GetAxis("MoveH") != 0)
-=======
-        if(player.GetAxis("MoveV") != 0 || player.GetAxis("MoveH") != 0)
->>>>>>> master
         {
             animator.SetBool("isMoving", true);
             Debug.Log("Moving");
         }
+        
       /*  else
         {
             animator.SetBool("isMoving", false);
