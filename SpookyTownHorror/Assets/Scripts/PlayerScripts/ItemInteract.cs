8a5f1c0b4b640990b85f895c83a703920e4b49f9
corrannolan <corrannolan3097@gmail.com>
2018-12-06 15:05:05 -0500

    DOOR WORK I SURE HOPE IT DOS

--- SpookyTownHorror/Assets/Scripts/PlayerScripts/ItemInteract.cs
+++ SpookyTownHorror/Assets/Scripts/PlayerScripts/ItemInteract.cs
@@ -1,22 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Rewired;
 
 public class ItemInteract : MonoBehaviour {
-    private GameObject item;
+    Player player;
+    public int playerNum;
+
+    public GameObject item;
+
+    Door door;
+    bool hasKey = false;
 
 	// Use this for initialization
 	void Start () {
-		
+        player = ReInput.players.GetPlayer(playerNum);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (player.GetButtonDown("Hide"))
+        {
+            if(item.tag == "Key")
+            {
+                hasKey = true;
+                item.SetActive(false);
+            }
+
+            if(item.tag == "Door")
+            {
+                door = item.GetComponent<Door>();
+                print("got door");
+
+                if(door.locked == true)
+                {
+                    if(hasKey == false)
+                    {
+                        //locked UI pop-up
+                    }
+                    else
+                    {
+                        door.locked = false;
+                    }
+                }
+
+                if(door.locked == false)
+                {
+                    door.Open();
+                    print("open door called");
+                }
+            }
+        }
 	}
 
     private void OnTriggerEnter(Collider other)
     {
-        item = other.gameObject;
+        if(other.tag == "Door" || other.tag == "Key")
+            item = other.gameObject;
     }
 }
