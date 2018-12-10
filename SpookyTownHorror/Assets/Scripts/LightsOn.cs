76495461a88a261434518772b9e338aee768338e
corrannolan <corrannolan3097@gmail.com>
2018-12-10 00:00:00 -0500

    yote

---
+++ SpookyTownHorror/Assets/Scripts/LightsOn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOn : MonoBehaviour {
+    public GameObject[] lights;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            foreach (GameObject light in lights)
+            {
+                light.GetComponent<Light>().enabled = true;
+            }
+        }
+    }
+}
