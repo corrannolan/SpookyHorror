2107feededc647a63349b4e0f5e6e05bcc39b6c6
Lucas <lucasdasil19@gmail.com>
2018-12-13 08:41:20 -0500

    spooky tv

---
+++ SpookyTownHorror/Assets/Activator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour {
+
+    public GameObject thing1;
+    AudioSource audioData;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       if (StoryManager.sm.AfterBasement == true)
+      {
+            thing1.gameObject.SetActive(true);
+        }
+    }
+
+        // Update is called once per frame
+        void Update () {
+		
+	}
+}
