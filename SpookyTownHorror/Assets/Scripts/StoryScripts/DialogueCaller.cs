65b099dff646488f36b823207636afb3ab14c49f
Allison Whitney <corvalys@gmail.com>
2018-12-13 04:23:37 -0500

    dialogue story manager is in and works

--- SpookyTownHorror/Assets/Scripts/StoryScripts/DialogueCaller.cs
+++ SpookyTownHorror/Assets/Scripts/StoryScripts/DialogueCaller.cs
@@ -11,7 +11,7 @@ public class DialogueCaller : MonoBehaviour {
 
     bool called = false;
 
-    public bool stopPlayer;
+    public bool stopPlayer, lastBox, floor1, floor2, yard;
 
 	// Use this for initialization
 	void Start () {
@@ -27,9 +27,69 @@ public class DialogueCaller : MonoBehaviour {
     {
         if (other.gameObject.tag == "Player")
         {
-            dM.line = startLine;
-            dM.stopLine = stopLine;
-            dM.Dialogue(stopPlayer);
+            if (yard)
+            {
+                dM.line = startLine;
+                dM.stopLine = stopLine;
+                dM.Dialogue(stopPlayer);
+            }
+            else if (StoryManager.sm.AfterBasement == false && (StoryManager.sm.emptyFloor1 == false && floor1) && gameObject.CompareTag("DialogueA"))
+            {
+                dM.line = startLine;
+                dM.stopLine = stopLine;
+                dM.Dialogue(stopPlayer);
+                if (lastBox)
+                {
+                    StoryManager.sm.emptyFloor1 = true;
+                }
+            }
+            else if (StoryManager.sm.AfterBasement == false && (StoryManager.sm.emptyFloor2 == false && floor2) && gameObject.CompareTag("DialogueA"))
+            {
+                dM.line = startLine;
+                dM.stopLine = stopLine;
+                dM.Dialogue(stopPlayer);
+                if (lastBox)
+                {
+                    StoryManager.sm.emptyFloor2 = true;
+                }
+            }
+            else if (StoryManager.sm.AfterBasement == true && (StoryManager.sm.emptyFloor1 == false && floor1) && gameObject.CompareTag("DialogueB"))
+            {
+                dM.line = startLine;
+                dM.stopLine = stopLine;
+                dM.Dialogue(stopPlayer);
+                if (lastBox)
+                {
+                    StoryManager.sm.emptyFloor1 = true;
+                }
+            }
+            else if (StoryManager.sm.AfterBasement == true && (StoryManager.sm.emptyFloor2 == false && floor2) && gameObject.CompareTag("DialogueB"))
+            {
+                dM.line = startLine;
+                dM.stopLine = stopLine;
+                dM.Dialogue(stopPlayer);
+                if (lastBox)
+                {
+                    StoryManager.sm.emptyFloor2 = true;
+                }
+            }
+            else if (StoryManager.sm.AfterBasement == false && StoryManager.sm.emptyBasement == false && gameObject.CompareTag("Dialogue"))
+            {
+                dM.line = startLine;
+                dM.stopLine = stopLine;
+                dM.Dialogue(stopPlayer);
+                if (lastBox)
+                {
+                    StoryManager.sm.emptyFloor1 = false;
+                    StoryManager.sm.emptyFloor2 = false;
+                    StoryManager.sm.emptyBasement = true;
+                    StoryManager.sm.AfterBasement = true;
+                }
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
             if (nextTrigger != null)
                 nextTrigger.SetActive(true);
             gameObject.SetActive(false);
