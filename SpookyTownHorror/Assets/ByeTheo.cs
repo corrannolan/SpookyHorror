dff83578fef527554a389af2ebc579628e51bf2e
Allison Whitney <corvalys@gmail.com>
2018-12-13 10:06:05 -0500

    no message

---
+++ SpookyTownHorror/Assets/ByeTheo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ByeTheo : MonoBehaviour {
+
+    public GameObject theo;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            theo.SetActive(false);
+        }
+    }
+}
